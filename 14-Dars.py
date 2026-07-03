0930c7d60bacc54c4b790fe7a3f5018a98e45182
Abdulloh <abdullohqodirov556@gmail.com>
2026-07-03 17:50:31 +0500

    14-Dars

---
+++ 14-Dars.py
@@ -0,0 +1,22 @@
+DISHES = [
+    "Pasta with tomato sauce",
+    "Chicken stir-fry",
+    "Borscht",
+    "Pancakes",
+    "Grilled cheese sandwich",
+]
+import random   
+
+
+
+#@dp.message(Command("list"))
+#async def cmd_list(message: types.Message):
+    #if not DISHES:
+        #await message.answer("The list is empty.")
+       # return
+
+    #text = "Here's what's in the list:\n"
+    #for i, dish in enumerate(DISHES, start=1):
+     #   text += f"{i}. {dish}\n"
+
+   # await message.answer(text)
